d37c6d8246ee6d8e8ec692ae27775438e0c6aa56
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-17 00:16:24 -0700

    all prefect

--- gitacp.py
+++ gitacp.py
@@ -1,22 +1,48 @@
 import subprocess
+from os.path import exists
 from sys import argv
 
 
 def git_linux(commit_message):
     """window git command function"""
-    commands = [
-        'git add .',
-        f'git commit -m "{commit_message}"',
-        'git push'
-    ]
+    commit_files = []
+    commit_groups = []
+    current_message  = []
 
-    for command in commands:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        if result.returncode != 0:
-            print(f"Error executing: {command}")
-            print(result.stderr)
-            return
-        print(result.stdout)
+    for i in commit_message:
+        if exists(i):
+            if commit_files and current_message:
+                commit_groups.append([' '.join(commit_files), ' '.join(current_message)])
+                commit_files = []
+                current_message = []
+            commit_files.append(i)
+        else:
+            current_message.append(i)
+
+    if commit_files or current_message:
+        commit_files = commit_files if commit_files else ['.']
+        commit_groups.append([' '.join(commit_files), ' '.join(current_message)])
+
+    for files, message in commit_groups:
+        commands = [
+            f'git add {files}',
+            f'git commit -m "{message}"',
+            'git push origin master'
+        ]
+
+        print("Adding files:")
+        for file_name in files.split(' '):
+            print(f"\t{file_name}")
+        print(f"\nCommit message: {message}")
+
+        for command in commands:
+            result = subprocess.run(command, shell=True, capture_output=True, text=True)
+            if result.returncode != 0:
+                print(f"Error executing: {command}")
+                print(result.stderr)
+                return
+            print(result.stdout)
+        print('-------------------------------------\n')
     return 'Done Push!'
 
 
@@ -25,5 +51,5 @@ if __name__ == '__main__':
         print("Usage: python script.py <commit_message>")
         exit(1)
 
-    commit_text = " ".join(argv[1:])
+    commit_text = argv[1:]
     print(git_linux(commit_text))
