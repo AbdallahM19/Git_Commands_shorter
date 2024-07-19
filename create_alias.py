9ee9c70601eca2e3435a7cd698f011aa9c0dda07
AbdallahM19 <abdallahmaabakar@gmail.com>
2024-07-19 06:48:05 -0700

    python3 ./create_alias.py ./gitacp_update.py

--- create_alias.py
+++ create_alias.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 import os
 
 
@@ -13,7 +14,13 @@ def code_subfuntion_run(line):
 
 
 if __name__ == '__main__':
-    alias_command = f'alias quickpush=\'python3 -u "{os.getcwd()}/gitacp.py"\''
+    alias_command, line = "", sys.argv[1]
+    if len(sys.argv) == 2:
+        if line.startswith('./'):
+            line = sys.argv[1][2:]
+        alias_command = f'alias quickpush=\'python3 -u "{os.getcwd()}/{line}"\''
+    else:
+        alias_command = f'alias quickpush=\'python3 -u "{os.getcwd()}/gitacp.py"\''
     command = f'echo "{alias_command}" >> ~/.bashrc'
     result = code_subfuntion_run(command)
     print("Alias created successfully.")
